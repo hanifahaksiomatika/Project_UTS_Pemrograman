bf0fd140a4b888f4f2c65f4def6dd366adcc9d4f
hanifahaksiomatika <128040561+hanifahaksiomatika@users.noreply.github.com>
2024-02-02 09:08:44 +0700

    Add files via upload

---
+++ projectuts/projectuts/Dosen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectuts
+{
+    class Dosen
+    {
+        public string Matakuliah { get; set; }
+        public string Nama { get; set; }
+        public int NIK { get; set; }
+
+        public Dosen()
+        {
+            // Default constructor
+        }
+
+        public void SetDosen(int nik, string nama)
+        {
+            NIK = nik;
+            Nama = nama;
+        }
+
+        public void SetMatakuliah(string matakuliah)
+        {
+            Matakuliah = matakuliah;
+        }
+
+    }
+}
---
+++ projectuts/projectuts/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectuts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen = new Dosen();
+
+            Console.WriteLine("Masukkan NIK dosen: ");
+            int nikDosen = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nMasukkan nama dosen: ");
+            string namaDosen = Console.ReadLine();
+
+            dosen.SetDosen(nikDosen, namaDosen);
+
+            Console.WriteLine("\nMasukkan Nama Matakuliah: ");
+            string namaMatakuliah = Console.ReadLine();
+
+            dosen.SetMatakuliah(namaMatakuliah);
+
+            Console.WriteLine("\n\nData Dosen:");
+            Console.WriteLine("====================");
+            Console.WriteLine("NIK        : " + dosen.NIK);
+            Console.WriteLine("Nama       : " + dosen.Nama);
+            Console.WriteLine("Matakuliah : " + dosen.Matakuliah);
+
+            Console.ReadLine();
+
+        }
+    }
+}
